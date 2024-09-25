f504677ea2a63930e8ec43b91ff51176f979b5a1
Maycon Wisley Batista <maycon.batista@seniornortepr.com.br>
2024-09-25 13:41:32 -0300

    Inclusão Timeout Cmd

---
+++ Timeout-Lsp-Cmd/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Timeout_Lsp_Cmd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                int timeout = int.Parse(args[0]);
+                for (int i = timeout; i > 0; i--)
+                {
+                    Console.WriteLine($"Aguardando... {i:00} segundos.");
+                    if (i == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Aguardando... {i:00} segundo.");
+                    }
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Aguardando... 01 segundo.");
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+    }
+}
